0830dcea3e5fa10191612b9c0388d7a0f9cbc91f
Vintha TR <trvinitha@gmail.com>
2020-06-11 17:15:37 +0200

    same line updated by bhpe

--- neg.c
+++ neg.c
@@ -7,14 +7,14 @@ void main(void)
 	signed char sc1,sc2;
 	signed short ss1;
 
-        sc1 = (signed char)-1;
+        sc1 = (signed char)-3;
 	sc2 = (signed char)-128;
 	ss1 = (signed short)-32768;
 
 	ui1 = 65535;
         us1 = 255;
 
-	printf(" -1 in hex is : %x\n", sc1);
+	printf(" -3 in hex is : %x\n", sc1);
 
 	printf(" -128  in hex  is : %x\n, size of char is : %d\n", (sc2 &0xff), sizeof(sc2));
 
@@ -24,4 +24,3 @@ void main(void)
 
 	printf(" -32768  signed short in hex is : %x\n, size of signed short  is : %d\n",ss1, sizeof(ss1));
 }
-// bhpe opened this file
