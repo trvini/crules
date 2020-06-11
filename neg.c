7e20bac4e1f2236a9fd75de55689eab25bb80c01
Vintha TR <trvinitha@gmail.com>
2020-06-11 13:50:16 +0200

    unsigned int added to code

--- neg.c
+++ neg.c
@@ -2,11 +2,17 @@
 
 void main(void)
 {
+	unsigned int ui_1;
 	signed int x,y;
+
 	x = (signed int)-1;
 	y = (signed int)0xffffff80;
+
+	ui_1 = 255;
 
 	printf(" -1 in hex is : %x\n", x);
 
 	printf(" 0x80 in decimal  is : %d\n",y);
+
+	printf(" 255 unsigned int in hex is : %x\n",ui_1);
 }
