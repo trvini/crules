958013ad1c2d758005976e520b1159c355e682f2
Vintha TR <trvinitha@gmail.com>
2020-06-11 17:05:19 +0200

    signed short added

--- neg.c
+++ neg.c
@@ -2,17 +2,25 @@
 
 void main(void)
 {
-	unsigned char uc1;
+	unsigned int ui1;
+	unsigned short us1;
 	signed char sc1,sc2;
+	signed short ss1;
 
         sc1 = (signed char)-1;
 	sc2 = (signed char)-128;
+	ss1 = (signed short)-32768;
 
-	uc1 = 255;
+	ui1 = 65535;
+        us1 = 255;
 
-	printf(" -1 in hex is : %4x\n", sc1);
+	printf(" -1 in hex is : %x\n", sc1);
 
-	printf(" -128  in hex  is : %x\n",sc2);
+	printf(" -128  in hex  is : %x\n, size of char is : %d\n", (sc2 &0xff), sizeof(sc2));
 
-	printf(" 255 unsigned int in hex is : %x\n",uc1);
+	printf(" 255 unsigned short in hex is : %x\n, size of short is : %d\n",us1, sizeof(us1));
+
+	printf(" 255 unsigned int in hex is : %x\n, size of int is : %d\n",ui1, sizeof(ui1));
+
+	printf(" -32768  signed short in hex is : %x\n, size of signed short  is : %d\n",ss1, sizeof(ss1));
 }
